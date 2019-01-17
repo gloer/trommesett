e7208a3d08ee63ad6cfadedd34b013f7046506f5
Gløer Olav Langslet <gloer@langslet.com>
2019-01-17 17:54:46 +0100

    Start files for JavaScript drummer

---
+++ index.js
@@ -0,0 +1,8 @@
+const btnRecord = document.querySelector("#btnRecord");
+const btnPlay = document.querySelector("#btnPlay");
+
+
+
+// document.addEventListener("keydown", play);
+// btnRecord.addEventListener("click", startOpptak);
+// btnPlay.addEventListener("click", spillOpptak);
